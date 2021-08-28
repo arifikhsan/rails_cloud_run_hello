f33f9982ae17175f46d58bf3793f84e39f0070b0
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-08-28 12:29:24 +0700

    hosts clear

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.hosts.clear
+  # config.hosts << /[a-z0-9]+\.run\.app/
 end
